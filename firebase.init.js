152b947f1bdb74da92fa4af415987d013804ad33
mohammedshojib <asmdshojib11@gmail.com>
2022-05-26 16:37:04 +0600

    firebase done

--- firebase.init.js
+++ firebase.init.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMI4CgGhYniDAcZqIolMVYuZZTPpB0Mi0",
-  authDomain: "microbyte-6d5f8.firebaseapp.com",
-  projectId: "microbyte-6d5f8",
-  storageBucket: "microbyte-6d5f8.appspot.com",
-  messagingSenderId: "365489331284",
-  appId: "1:365489331284:web:f1b5da9d32da1e4592a1c1",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
